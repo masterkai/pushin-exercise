a1edf023e9d793d391a6a3cf604f3cfe974f8146
masterkai <kai0919@hotmail.com>
2019-10-28 00:44:43 +0800

    Desktop version complete

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,130 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div>
+        <img src="../assets/pigP.gif" ref="pig" class="pig"/>
+        <img src="../assets/0_Horse.png" ref="Horse" class="Horse"/>
+        <img src="../assets/0_Rabbit.png" ref="Rabbit" class="Rabbit"/>
+        <img src="../assets/0_Cow.png" ref="Cow" class="Cow"/>
+        <img src="../assets/0_Sheep.png" ref="Sheep" class="Sheep"/>
+        <img src="../assets/風車.gif" class="windmill"/>
+        <img src="../assets/大標.png" class="Subject"/>
+
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  import {TimelineMax} from 'gsap'
+
+  export default {
+    mounted() {
+      const {pig, Horse, Rabbit, Cow, Sheep} = this.$refs
+
+      const timeline = new TimelineMax()
+      timeline.set(pig, {y: '+=10', autoAlpha: 0})
+      timeline.set(Horse, {y: '+=10', autoAlpha: 0})
+      timeline.set(Rabbit, {y: '+=10', autoAlpha: 0})
+      timeline.set(Cow, {y: '+=10', autoAlpha: 0})
+      timeline.set(Sheep, {y: '+=10', autoAlpha: 0})
+
+      timeline.to(Horse, 0, {autoAlpha: 1, rotation: -5})
+      timeline.to(Rabbit, 0, {autoAlpha: 1})
+      timeline.to(Cow, 0, {autoAlpha: 1, rotation: -5})
+      timeline.to(Sheep, 0, {autoAlpha: 1, rotation: -5})
+
+      timeline.to(pig, 1, {autoAlpha: 1})
+
+
+      timeline.to(pig, .25, {
+        css: {y: -50, transformOrigin: '50% 50%'},// remove default easing for seamless loop
+        repeatDelay: null,
+        repeat: -1,
+        yoyo: true,
+      })
+      timeline.to(pig, 5, {
+          css: {x: 1400, transformOrigin: '50% 50%'},// remove default easing for seamless loop
+          repeatDelay: null,
+          repeat: -1,
+          yoyo: false,
+        }
+      )
+
+      timeline.to(Horse, 1, {
+        css: {rotation: 5, transformOrigin: '50% 50%'},// remove default easing for seamless loop
+        repeatDelay: null,
+        repeat: -1,
+        yoyo: true,
+      })
+
+      timeline.to(Rabbit, 1, {
+        css: {y: -10, transformOrigin: '50% 50%'},// remove default easing for seamless loop
+        repeatDelay: null,
+        repeat: -1,
+        yoyo: true,
+      })
+
+      timeline.to(Sheep, 1, {
+        css: {rotation: 5, transformOrigin: '50% 50%'},// remove default easing for seamless loop
+        repeatDelay: null,
+        repeat: -1,
+        yoyo: true,
+      })
+
+      timeline.to(Cow, 1, {
+        css: {rotation: 5, transformOrigin: '50% 50%'},// remove default easing for seamless loop
+        repeatDelay: null,
+        repeat: -1,
+        yoyo: true,
+      })
+
+
+    }
   }
-}
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style>
+    .Subject {
+      position: absolute;
+      left: 50%;
+      transform: translateX(-50%);
+      z-index: 11;
+    }
+
+    .Horse {
+        position: absolute;
+        top: 790px;
+        left: 50px;
+    }
+
+    .Rabbit {
+        position: absolute;
+        top: 768px;
+        left: 526px;
+    }
+
+    .Cow {
+        position: absolute;
+        top: 800px;
+        right: 430px;
+    }
+
+    .Sheep {
+        position: absolute;
+        top: 830px;
+        right: 50px;
+    }
+
+    .pig {
+        position: absolute;
+        top: 600px;
+        /*display: none;*/
+        height: auto;
+        max-width: 100%;
+        z-index: 10;
+    }
+
+    .windmill {
+        position: absolute;
+        top: 0;
+    }
+
+
 </style>
---
+++ src/components/red-box-markup.vue
@@ -0,0 +1,11 @@
+<template>
+    <div ref="box" class="box"></div>
+</template>
+
+<style>
+    .box {
+        height: 60px;
+        width: 60px;
+        background: red;
+    }
+</style>
